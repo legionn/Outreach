1732e3400155d133b70f4bdb8f3f413be3b60bfd
Jonathan Boulanger <boulajp@mcmaster.ca>
2015-07-29 10:22:36 -0400

    changed shapes

--- tutorials/shapes/input.elm
+++ tutorials/shapes/input.elm
@@ -3,19 +3,15 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
 
-main : Element
-main =
-  collage 300 300
-    [ ngon 4 75
-        |> filled clearGrey
-        |> move (-10,0)
-    , ngon 5 50
-        |> filled clearGrey
-        |> move (50,10)
-    ]
+main = Signal.map view Window.dimensions
+
+view (w,h) = collage w h [forms]
 
 
-clearGrey : Color
-clearGrey =
-  rgba 111 111 111 0.6
 
+
+
+
+-- This is where you add your shapes
+
+forms = group []
